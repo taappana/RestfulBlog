f660b294c888ea64f39dfda849ca150b7cbcf1df
Jani Herranen <herranen.jani@gmail.com>
2017-04-09 18:26:07 +0000

    added NEW  and CREATE routes

--- app.js
+++ app.js
@@ -24,14 +24,29 @@ app.get("/", (req, res) => {
     res.redirect("/blogs"); 
 });
 
+// INDEX RUOTE
 app.get("/blogs", (req, res) => {
     Blog.find({}, (err, blogs) => {
         if(err){
             console.log("ERROR!!!");
-        }   {
-            res.render("index", {blogs: blogs}); 
         }
+            res.render("index", {blogs: blogs}); 
     })
+});
+
+// NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err){
+            res.render("new");
+        }
+            res.redirect("/blogs");    
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
